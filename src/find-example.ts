de8bc4af71111f5a42a0d07de53a1dd09eea72a1
Jeremy Cherer <chererjeremy@gmail.com>
2024-02-01 12:56:54 -0700

    enhancement Add lines to the file path
    aded option to add line number

--- src/find-example.ts
+++ src/find-example.ts
@@ -2,18 +2,43 @@ import { existsSync, readFileSync } from 'fs';
 import { Comment } from 'typedoc';
 
 export function findExample(comment: Comment): string | null {
-	// Check for the @includeExample tag and get its associated file path
-	const includeExampleTag = comment.blockTags.find(tag => tag.tag === "@includeExample");
-	if (!includeExampleTag) {
-		return null;
-	}
+    // Check for the @includeExample tag and get its associated file path
+    const includeExampleTag = comment.blockTags.find((tag: { tag: string; }) => tag.tag === "@includeExample");
+    if (!includeExampleTag) {
+        return null;
+    }
+    const filePathWithLines = includeExampleTag.content[0].text;
+    const [filePath, lineNumbersString] = filePathWithLines.split(':');
 
-	const filePath = includeExampleTag.content[0].text;
+    // Ensure file exists
+    if (!existsSync(filePath)) {
+        throw new Error(`File not found for ${filePath}`);
+    }
 
-	// Ensure file exists
-	if (!existsSync(filePath)) {
-	    throw new Error(`File not found for ${filePath}`);
-	}
+    const fileContent = readFileSync(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
 
-	return readFileSync(filePath, 'utf-8');
+    // If no line numbers are specified, return the whole file
+    if (!lineNumbersString) {
+        return fileContent;
+    }
+
+    const lineNumbers = lineNumbersString.split(',').map((line: string) => line.includes('-') ? line.split('-').map(Number) : Number(line));
+
+    // Ensure line numbers are within file
+    lineNumbers.forEach((lineNumber: number | any[]) => {
+        if (Array.isArray(lineNumber)) {
+            if (lineNumber[0] < 1 || lineNumber[1] > lines.length) {
+                throw new Error(`Line numbers ${lineNumber[0]}-${lineNumber[1]} are out of range for file ${filePath}`);
+            }
+        } else {
+            if (lineNumber < 1 || lineNumber > lines.length) {
+                throw new Error(`Line number ${lineNumber} is out of range for file ${filePath}`);
+            }
+        }
+    });
+
+    const selectedLines = lineNumbers.flatMap((lineNumber: number | (number | undefined)[]) => Array.isArray(lineNumber) ? lines.slice(lineNumber[0] - 1, lineNumber[1]) : lines[lineNumber - 1]);
+
+    return selectedLines.join('\n');
 }
