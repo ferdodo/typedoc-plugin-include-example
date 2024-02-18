75cc69363e4c52ff8f86b7c5688bfa3aeccfc5e2
Thomas Riffard <ferdodo@hotmail.fr>
2024-02-18 10:11:07 +0100

    remove extra line break

--- src/include-example.ts
+++ src/include-example.ts
@@ -3,7 +3,7 @@ import { Comment } from "typedoc";
 export function includeExample(comment: Comment, example: string): void {
 	comment.summary.push({
 		kind: "code",
-		text: `\n\n### Example\n\`\`\`\n\n${example}\n\`\`\``
+		text: `\n\n### Example\n\`\`\`\n${example}\n\`\`\``
 	});
 
 	comment.blockTags = comment.blockTags.filter(tag => tag.tag !== "@includeExample");
