deec5b6b08e1ba1846eb41b59cff82a1765faba8
Thomas Riffard <ferdodo@hotmail.fr>
2024-02-18 19:41:23 +0100

    remove comments

--- src/find-example.ts
+++ src/find-example.ts
@@ -5,7 +5,6 @@ import { parseIncludeExampleTag } from "./parse-include-example-tag";
 import { applyLineSelection } from "./apply-line-selection";
 
 export function findExample(comment: Comment): string | null {
-	// Check for the @includeExample tag and get its associated file path
 	const commentTag = comment.blockTags.find((tag: { tag: string }) => tag.tag === "@includeExample");
 
 	if (!commentTag) {
@@ -14,7 +13,6 @@ export function findExample(comment: Comment): string | null {
 
 	const includeExampleTag: IncludeExampleTag = parseIncludeExampleTag(commentTag.content[0].text);
 
-	// Ensure file exists
 	if (!existsSync(includeExampleTag.path)) {
 		throw new Error(`File not found for ${includeExampleTag.path}`);
 	}
