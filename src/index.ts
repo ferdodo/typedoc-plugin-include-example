21e610056c04598517ea7189b3f21d261a380b98
Thomas Riffard <ferdodo@hotmail.fr>
2024-11-06 12:17:14 +0100

    v1.4.1 Set config before it's frozen

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { Converter, Application } from "typedoc";
 import { processComments } from "./process-comments";
 
 export function load(application: Application) {
-	application.options.setValue("blockTags", ["@includeExample"]);
+	application.on(Application.EVENT_BOOTSTRAP_END, () => {
+		application.options.setValue("blockTags", [
+			...new Set([
+				...application.options.getValue("blockTags"),
+				"@includeExample"
+			])
+		]);
+	});
+
 	application.converter.on(Converter.EVENT_RESOLVE_BEGIN, processComments);
 }
